d8f95c438cea814b75c7e2fd2983b18d20559dbf
geenva <geenva@users.noreply.github.com>
2021-05-21 20:42:33 +0800

    feat: verify

---
+++ src/bot/commands/util/verify.ts
@@ -0,0 +1,31 @@
+import Command from "../../struct/Command";
+import { Message } from "discord.js";
+
+abstract class Verify extends Command {
+  constructor() {
+    super({
+      name: "!verify",
+      aliases: ["!v"],
+      description: "Verify user",
+      requiredArgs: 1,
+      userPermissions: ["MANAGE_ROLES"],
+    });
+  }
+
+  exec(message: Message) {
+    const member = message.mentions.members!.first();
+    const role = message.guild!.roles.cache.find(
+      (role) => role.id == "845139669699133450"
+    )!;
+    member!.roles.add(role).then(() => {
+      message.channel.send("Succeeded!").then((m) => {
+        setTimeout(() => {
+          m.delete();
+          message.delete();
+        }, 3000);
+      });
+    });
+  }
+}
+
+export default Verify;
