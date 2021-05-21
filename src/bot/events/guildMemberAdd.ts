cc649ce4c7b3fe1982fa363d9387e12cdcea6d1b
geenva <geenva@users.noreply.github.com>
2021-05-21 20:09:48 +0800

    feat: @ user on join

---
+++ src/bot/events/guildMemberAdd.ts
@@ -0,0 +1,22 @@
+import Event from "../struct/Event";
+import { GuildMember } from "discord.js";
+
+abstract class GuildMemberAdd extends Event {
+  constructor() {
+    super({
+      name: "guildMemberAdd",
+    });
+  }
+
+  async exec(member: GuildMember) {
+    const channel = member.guild.channels.cache.get("845141656343674881");
+
+    if (channel!.isText()) {
+      channel.send("<@" + member.user.id + ">").then((m) => {
+        m.delete();
+      });
+    }
+  }
+}
+
+export default GuildMemberAdd;
