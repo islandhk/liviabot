c881582adbcb4e692caf7f5954106c4cfd8c5364
geenva <geenva@users.noreply.github.com>
2021-05-22 11:20:13 +0800

    e

--- src/bot/events/message.ts
+++ src/bot/events/message.ts
@@ -1,5 +1,5 @@
 import Event from "../struct/Event";
-import { Message, TextChannel, Guild, Collection } from "discord.js";
+import { Message, TextChannel, Guild, Collection, DMChannel } from "discord.js";
 import settings from "../settings";
 
 abstract class MessageEvent extends Event {
@@ -134,7 +134,12 @@ abstract class MessageEvent extends Event {
             "This command can only be used in a guild."
           );
         } else if (command.cultOnly) {
-          return;
+          if (message.guild instanceof DMChannel) return;
+          if (message.guild instanceof Guild) {
+            if (message.guild.id !== settings.CULT_ID) {
+              return;
+            }
+          }
         }
 
         if (message.channel instanceof TextChannel) {
