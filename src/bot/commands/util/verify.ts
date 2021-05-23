870404a58988cb197eb312880c82daa827346c31
geenva <geenva@users.noreply.github.com>
2021-05-23 17:43:31 +0800

    feat: delete messages by user after verification

--- src/bot/commands/util/verify.ts
+++ src/bot/commands/util/verify.ts
@@ -15,16 +15,28 @@ abstract class Verify extends Command {
 
   exec(message: Message) {
     const member = message.mentions.members!.first();
+
     const role = message.guild!.roles.cache.find(
       (role) => role.id == "845139669699133450"
     )!;
+
     member!.roles.add(role).then(() => {
-      message.channel.send("Succeeded!").then((m) => {
-        setTimeout(() => {
-          m.delete();
-          message.delete();
-        }, 3000);
-      });
+      message.channel.messages
+        .fetch({
+          limit: 100, // Change `100` to however many messages you want to fetch
+        })
+        .then((messages) => {
+          messages
+            .filter((m) => m.author.id === member?.user.id)
+            .forEach((msg) => msg.delete());
+        });
+    });
+
+    message.channel.send("Succeeded!").then((m) => {
+      setTimeout(() => {
+        m.delete();
+        message.delete();
+      }, 3000);
     });
   }
 }
