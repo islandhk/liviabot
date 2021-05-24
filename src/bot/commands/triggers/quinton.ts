639c14305249f5405a7f8b59d2a3479fa597296b
geenva <geenva@users.noreply.github.com>
2021-05-24 20:52:29 +0800

    ok

--- src/bot/commands/triggers/quinton.ts
+++ src/bot/commands/triggers/quinton.ts
@@ -12,7 +12,7 @@ abstract class Quinton extends Command {
 
   exec(message: Message) {
     return message.channel.send(
-      "Quinton. Stop watching Ball Stars vido. It is inaproprate to watch these vidos in glass because we are focusing. I will be emailing your parents shortly abotu this issue. Sebashun. Cannot talk to Quinton."
+      "Quinton. Stop watching Ball Stars vido. It is inaproprate to watch these vidos in glass because we are focusing. I will be emailing your parents shortly abotu this issue. Also you are not supposed to be sitting with Hayley and Jespar. Or Sebashun. Sebashun Cannot talk to Quinton."
     );
   }
 }
