f947db73e362c22953002b96b5087f3e2243e283
geenva <geenva@users.noreply.github.com>
2021-06-17 17:02:20 +0800

    🔥 Remove unused import

--- src/bot/commands/livia/lyrics.ts
+++ src/bot/commands/livia/lyrics.ts
@@ -1,5 +1,5 @@
 import Command from "../..//struct/Command";
-import { Message, MessageEmbed } from "discord.js";
+import { Message } from "discord.js";
 import fs from "fs";
 
 abstract class Lyrics extends Command {
